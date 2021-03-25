d108cab44e5b4d12cc3b1f72d0a9d2c7cc9abfe0
pedroshaw <p_shaw@btconnect.com>
2021-03-25 13:41:20 +0000

    Assignment to add recipe, ingredients and notes. Display recipe description on index page

---
+++ src/main/java/guru/springframework/services/RecipeService.java
@@ -0,0 +1,13 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+
+import java.util.Set;
+
+/**
+ * Created by: p_sha on: Thu 25 Mar 2021
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
